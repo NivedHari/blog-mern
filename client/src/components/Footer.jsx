2915d594edb89402cd8b4d06ed534238876690d2
NivedHari <nivedhari44@gmail.com>
2024-05-16 11:55:29 +0530

    Sign up functionality and footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,92 @@
+import {
+  Footer,
+  FooterCopyright,
+  FooterDivider,
+  FooterIcon,
+  FooterLink,
+  FooterLinkGroup,
+  FooterTitle,
+} from "flowbite-react";
+import { Link } from "react-router-dom";
+import {BsFacebook, BsInstagram, BsTwitterX, BsYoutube} from 'react-icons/bs';
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid-w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                TypoTales
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <FooterTitle title="About" />
+              <FooterLinkGroup col>
+                <FooterLink
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  TypoTales Blog
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+            <div>
+              <FooterTitle title="Follow us" />
+              <FooterLinkGroup col>
+                <FooterLink
+                  href="https://www.github.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </FooterLink>
+                <FooterLink
+                  href="https://www.github.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Facebook
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+            <div>
+              <FooterTitle title="Follow us" />
+              <FooterLinkGroup col>
+                <FooterLink
+                  href="#"
+                >
+                  Privacy Policy
+                </FooterLink>
+                <FooterLink
+                  href="#"
+                >
+                  Terms and Conditions
+                </FooterLink>
+              </FooterLinkGroup>
+            </div>
+          </div>
+        </div>
+        <FooterDivider/>
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <FooterCopyright href="#" by="TypoTales Blog" year={new Date().getFullYear()}/>
+          <div className="flex sm:mt-0 mt-4 gap-6 items-center">
+
+          <FooterIcon href="#" icon={BsFacebook}/>
+          <FooterIcon href="#" icon={BsInstagram}/>
+          <FooterIcon href="#" icon={BsYoutube}/>
+          <FooterIcon href="#" icon={BsTwitterX}/>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,49 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.username || !formData.password) {
+      return setErrorMessage("Please fill out all the fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      setSuccessMessage("Sign up successfull !");
+      if (res.ok) {
+        setTimeout(() => {
+          navigate("/sign-in");
+        }, 1000);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -20,21 +62,47 @@ export default function Signup() {
         </div>
         <div className="flex-1">
           {/* right */}
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Email" />
-              <TextInput type="text" placeholder="example@abc.com" id="email" />
+              <TextInput
+                type="email"
+                placeholder="example@abc.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -43,6 +111,16 @@ export default function Signup() {
               Sign in
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+          {successMessage && (
+            <Alert className="mt-5" color="success">
+              {successMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
